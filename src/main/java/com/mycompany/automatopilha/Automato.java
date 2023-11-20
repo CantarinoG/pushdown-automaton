3a891433180670d2a50958e929f3a3d274397d99
CantarinoG <guilhermecantarino1709@gmail.com>
2023-11-20 18:14:34 -0300

    Add Automato class base

---
+++ src/main/java/com/mycompany/automatopilha/Automato.java
@@ -0,0 +1,11 @@
+package com.mycompany.automatopilha;
+
+import java.util.ArrayList;
+
+public class Automato {
+    
+    private String estadoInicial;
+    private ArrayList<String> estadosFinais = new ArrayList<>();
+    //private ArrayList<Transicao> listaTransicoes = new ArrayList<>();
+    
+}
