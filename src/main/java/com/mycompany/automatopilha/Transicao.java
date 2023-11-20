10fc7ab5cffbd54a379fa81dac96f03c22a424da
CantarinoG <guilhermecantarino1709@gmail.com>
2023-11-20 18:17:47 -0300

    Add Transicao class

---
+++ src/main/java/com/mycompany/automatopilha/Transicao.java
@@ -0,0 +1,11 @@
+package com.mycompany.automatopilha;
+
+public class Transicao {
+    
+    public String de;
+    public String para;
+    public char letraFita;
+    public String leitura;
+    public String escrita;
+    
+}
